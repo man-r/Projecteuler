ad9a3dda81985d14839dd4561f97b245d45bb7e2
man-r <man-r@github.com>
2015-12-12 01:36:55 +0300

    correction for the p01

--- problem01 Multiples of 3 and 5/Solution.java
+++ problem01 Multiples of 3 and 5/Solution.java
@@ -4,23 +4,23 @@ class Solution  {
 
   public static void main(String[] args) {
     int max = Integer.parseInt(args[0]);
-    ArrayList<Integer> list = new ArrayList<Integer>();
-    
-    for (int i = 1; (i * 3) < max || (i * 5) < max; i++) {
+
+    int result = 0;
+    for (int i = 1; (i * 3) < max; i++) {
       int mThree = i * 3;
       int mFive = i * 5;
 
-      if (mThree < max) {
-        list.add(mThree);
+      if (mThree < max && mThree % 5 !=0) {
+        result = result + mThree;
       }
 
       if(mFive < max) {
-        list.add(mFive);
+        result = result + mFive;
       }
     }
 
     // Display the contents of the array list
-    System.out.println("The arraylist contains the following elements: " + list);
+    System.out.println("result: " + result);
   }
 
 }

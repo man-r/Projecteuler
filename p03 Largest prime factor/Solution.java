204a099786e202ce06a5816c3146a247d1af731c
man-r <man-r@github.com>
2015-12-12 15:11:26 +0300

    adding p03 solution

--- p03 Largest prime factor/Solution.java
+++ p03 Largest prime factor/Solution.java
@@ -1,37 +1,93 @@
 import java.util.*;
+import java.math.BigInteger;
 
 class Solution {
 
   public static void main(String[] args) {
-    int num = Integer.parseInt(args[0]);
+    System.out.println("largest factor: " + getLargestFactor(new BigInteger(args[0])));
+    System.out.println("largest factor: " + getLargestFactor(Integer.parseInt(args[0])));
+  }
 
-    ArrayList<Integer> factors = new ArrayList<Integer>();
+  static int getLargestFactor(int num) {
 
-    if (!isPrime(num)) {
-      if (num % 2 == 0) {
-        factors.add(2);
-      }
-      for (int i = 3; i <= num/2; i+=2) {
-        if (isPrime(i)) {
-          if (num % i == 0) {
-            factors.add(i);
-          }
+    int largestFactor = -1;
+
+    if (isPrime(num)) {
+      largestFactor = num;
+    }
+
+    else {
+      for (int i = 2; i < num/2 && largestFactor == -1; i++) {
+
+        if (isPrime(i) && num % i == 0) {
+            largestFactor = i;
         }
       }
+
+      int largestFactor2 = getLargestFactor(num/largestFactor);
+      if (largestFactor2 > largestFactor) {
+        largestFactor = largestFactor2;
+      }
     }
-    // Display the contents of the array list
-    System.out.println("The arraylist contains the following elements: " + factors);
+
+    return largestFactor;
+  }
+
+
+  static BigInteger getLargestFactor(BigInteger num) {
+
+    BigInteger largestFactor = BigInteger.ONE;
+
+    if (isPrime(num)) {
+      largestFactor = num;
+    }
+
+    else {
+
+      BigInteger i = new BigInteger("2");
+      BigInteger max = num.divide(i);
+
+      while(i.min(max).equals(i) && largestFactor.equals(BigInteger.ONE)) {
+
+        if (isPrime(i) && num.mod(i).equals(BigInteger.ZERO)) {
+          largestFactor = i;
+        }
+        i = i.add(BigInteger.ONE);
+      }
+
+      BigInteger largestFactor2 = getLargestFactor(num.divide(largestFactor));
+      if (largestFactor.min(largestFactor2).equals(largestFactor)) {
+        largestFactor = largestFactor2;
+      }
+    }
+
+    return largestFactor;
   }
 
 
   static boolean isPrime(int p) {
     for (int i = 2; i <= p/2; i++) {
       if (p % i == 0) {
-        System.out.println("not prime");
         return false;
       }
     }
 
+    return true;
+  }
+
+
+  static boolean isPrime(BigInteger p) {
+
+    BigInteger i = new BigInteger("2");
+    BigInteger max = p.divide(i);
+
+    while(i.min(max).equals(i)) {
+      if (p.mod(i).equals(BigInteger.ZERO)) {
+        return false;
+      }
+
+      i = i.add(BigInteger.ONE);
+    }
     return true;
   }
 

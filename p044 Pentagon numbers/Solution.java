a481b45ca74ece947714090071f9912aa499d942
man-r <man-r@github.com>
2016-03-16 01:30:22 +0300

    adding p044 solution

---
+++ p044 Pentagon numbers/Solution.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+  public static void main(String[] args) {
+    long startTime = System.currentTimeMillis();
+
+    ArrayList<Integer> pentagon = new ArrayList<Integer>();
+    double k = 0;
+    double j = 0;
+    boolean found = false;
+    pentagon.add(1);
+    pentagon.add(5);
+
+    for (int n = 1; !found; n++) {
+      pentagon.add(((n * ((3*n) - 1))/2));
+      int lastItem = pentagon.get(pentagon.size() - 1);
+
+      for (int i = 0; !found && i < pentagon.size(); i++) {
+        int sum = pentagon.get(i) + lastItem;
+        int difference = lastItem - pentagon.get(i);
+        if (pentagon.contains(difference)) {
+          for (int m = n + 1; !found && sum >=((m * ((3*m) - 1))/2); m++) {
+            if (sum == ((m * ((3*m) - 1))/2)) {
+              System.out.println("Pk: " + lastItem);
+              System.out.println("Pj: " + pentagon.get(i));
+              System.out.println("Pk - Pj: " + difference);
+              found = true;
+            }
+          }
+        }
+      }
+    }
+    System.out.println("totalTime: " + (System.currentTimeMillis() - startTime) + " milliseconds");
+  }
+
+  static boolean isPentagon(ArrayList<BigInteger> pentagon) {
+
+    return false;
+  }
+}
